1cd068f0edf9d85e4555f5d8f09d8aa51b249a8e
memoryfile <yuiotp999@protonmail.com>
2022-05-20 16:14:58 -0400

    Define computerPlay, handle ties, set computer and player scores

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
+console.log("Hello World")
+
+// Score at 0 initially, going for five rounds
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Each round determines a winner
+
+let winner = '';
+
+// When it's a tie
+
 function playRound(playerSelection, computerSelection) {
-    return computerResponse();
+    if (playerSelection === computerSelection) {
+        winner = 'none'
 }
 
-function computerResponse() {
-    const playerSelection = "rock";
-    const computerSelection = computerPlay();
-    return playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
+// Deciding computer's response 
+
+function computerPlay() {
+    let computerNumber = random(3);
+    let computerInput = '';
 }
 
-function playerSelection() {
-    const input = prompt("Do you play rock, paper, or scissors?");
-    alert('My pick is ${input}');
+switch (ComputerNumber) {
+    case a:
+        computerInput = 'rock';
+        break;
+    case b:
+        computerInput = 'paper'
+        break;
+    case c:
+        computerInput = 'scissors'
+        break;
 }
+
+// function computerResponse() {
+//     const playerSelection = "rock";
+//     const computerSelection = computerPlay();
+//     return playRound(playerSelection, computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+// }
+
+// function playerSelection() {
+//     const input = prompt("Do you play rock, paper, or scissors?");
+//     alert('My pick is ${input}');
+// }
+
+// console.log(playerSelection)
