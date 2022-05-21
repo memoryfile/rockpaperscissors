135d73a153814d06eedb1bb936a147a8c905b256
memoryfile <yuiotp999@protonmail.com>
2022-05-21 19:15:37 -0400

    Fix script, connect buttons to eventListeners, etc

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
-console.log("Hello World")
+console.log("Hello World");
+
+const displayRound = document.getElementById("round");
+const displayPlayerScore = document.getElementById("playerScore");
+const displayComputerScore = document.getElementById("computerScore");
+const buttons = document.querySelectorAll("button");
 
 // Score at 0 initially, going for five rounds
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 1;
 
-// Each round determines a winner
+// Let each round determine a winner
 
 let winner = '';
 
-// Start game through a prompt
+// Correspond rounds and scores to buttons
 
-let userPick = prompt("Do you pick rock, paper, or scissors?");
+// const displayRound = document.getElementById("round");
+// displayRound.innerHTML = "Round: " + round;
 
-if (userPick.toLowerCase() == "rock" || userPick.toLowerCase() == "scissors" || userPick.toLowerCase() == "rock") {
-  function playRound();
-}
+// const displayPlayerScore = document.getElementById("playerScore");
+// displayPlayerScore.innerHTML = "Your score: " + playerScore;
+
+// const displayComputerScore = document.getElementById("computerScore");
+// displayComputerScore.innerHTML = "Computer score: " + computerScore;
+
+// const buttons = document.querySelectorAll("button");
+
+// Event listeners for buttons 
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // What the user picks
+    const playerSelection = this.textContent;
+    alert("You picked " + playerSelection);
+    computerPlay();
+    alert("The computer played: " + computerSelection);
+    playRound();
+    gameOver();
+  });
 
     // Deciding computer's response
 
     function computerPlay() {
-        let computerNumber = random(3);
+        let computerNumber = Math.floor(Math.random(3));
         let computerSelection = '';
 
         switch (computerNumber) {
             case 1: computerSelection = 'rock';
                 break;
-            case 2: computerSelection = 'paper'
+            case 2: computerSelection = 'paper';
                 break;
-            case 3: computerSelection = 'scissors'
+            case 3: computerSelection = 'scissors';
                 break;
         }
 
@@ -39,30 +63,42 @@ if (userPick.toLowerCase() == "rock" || userPick.toLowerCase() == "scissors" || 
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        winner = 'nobody'
-        console.log("The winner is" + winner)
+        winner = 'nobody';
+        alert("The winner is" + winner);
+        console.log("The winner is" + winner);
     }
 
     // When the computer wins through matchups
 
     if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'scissors')) {
         computerScore++
-        winner = 'computer'
-        console.log("The winner is" + winner)
+        winner = 'computer';
+        alert("The winner is" + winner);
+        console.log("The winner is" + winner);
+        computerScore++
     }
 
     // When the player wins through matchups
 
     if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
         playerScore++
-        winner = 'player'
-        console.log("The winner is" + winner)
+        winner = 'player';
+        alert("The winner is" + winner);
+        console.log("The winner is" + winner);
+        playerScore++
     }
 
     // Determine when someone reaches a score of 5.
     // How can I get it to check the score after every round?
 
     function gameOver() {
-        return playerScore === 5 || computerScore === 5
+        if (playerScore === 5) {
+            return alert("Game over! You won!!");
+        } else if (computerScore === 5) {
+            return alert("Game over! You lost...");
+        } else {
+            alert("Continue to the next match!");
+        }
     }
 }
+});  
